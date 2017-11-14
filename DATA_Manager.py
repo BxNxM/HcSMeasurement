fb5c1f93a23b320033688f0fe07c4f2ed328c6e0
Bán Marcell <bnm@Bans-MacBook-Pro.local>
2017-11-14 12:25:33 +0100

    DATA managing improved - FIST RELEASE 1.0

--- DATA_Manager.py
+++ DATA_Manager.py
@@ -1,33 +1,165 @@
 from moduls.DATAlib import *
 import moduls.DATAlib as Dlib
+import moduls.Loggerlib as Loggerlib
+
+'''
+::: DATABASE INDEXING :::
+Upload database here
+i = id
+id 0 - Watertemp
+id 1 - Airtemp
+id 2 - Humidity
+id 3 - PH
+id 4 - EC
+id 5 - Light
+id 6 - Waterlvl
+id 7 - DataBaseSize
+'''
 
 dataFileList=None
+# ::: SET READING PERIOD HERE :::
+read_period = 2         # sec
+
+def Watertemp_read():
+    data_id = 0
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def Airtemp_read():
+    data_id = 1
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def Humidity_read():
+    data_id = 2
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def PH_read():
+    data_id = 3
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def EC_read():
+    data_id = 4
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def Light_read():
+    data_id = 5
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def Waterlvl_read():
+    data_id = 6
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+def DataBaseSize_read():
+    data_id = 7
+    # --- GET TEST VALUE ---
+    # SET REAL SENSOR READING HERE TAKE THE SENSOR VALUE TO VALUE VARIABLE - AND YOU ARE DONE :)
+    try:
+        value = float(dataFileList[data_id].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10
+    except Exception as e:
+        Loggerlib.DataLog.logger.critical(e)
+        value = randint(-1, 1)
+    # ----------------------
+    return value, data_id
+
+
+def debug_print(text, state=False):
+    if state:
+        print(str(text))
 
 def Refresh_dataBase():
-    global dataFileList
-
-    '''
-    Upload database here
-    i = id
-    id 0 - Watertemp
-    id 1 - Airtemp
-    id 2 - Humidity
-    id 3 - PH
-    id 4 - EC
-    id 5 - Light
-    id 6 - Waterlvl
-    id 7 - DataBaseSize
-    '''
+    global dataFileList, read_period
 
     while True:
-        #print("TEST DATAs IS GENERATING")
-        for i in range(0, len(dataFileList) - 1):
-            try:
-                dataFileList[i].AddData_WithTimestamp(float(dataFileList[i].ReadData_ToDict(1)[0, 'value']) + randint(-1, 1)/10)
-            except KeyError:
-                dataFileList[i].AddData_WithTimestamp(randint(-1, 1))
+        debug_print("TEST DATAs IS GENERATING")
 
-        time.sleep(2)
+        output_tuple = Watertemp_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor Watertemp index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = Airtemp_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor Airtemp index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = Humidity_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor Humidity index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = PH_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor PH index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = EC_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor EC index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = Light_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor Light index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = Waterlvl_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor Waterlvl index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        output_tuple = DataBaseSize_read()
+        dataFileList[output_tuple[1]].AddData_WithTimestamp(output_tuple[0])
+        debug_print("\tSensor Database index: {} value: {}".format(output_tuple[1], output_tuple[0]))
+
+        time.sleep(read_period)
 
 def main():
     global dataFileList
